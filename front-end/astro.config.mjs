d89093a445822d95233f63649e8cd77c5183ecd7
i.matheuslmaia <i.matheuslmaia@gmail.com>
2025-06-14 20:49:08 +0000

    subindo configuração inicial do astro

---
+++ front-end/astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable React to support React JSX components.
+  integrations: [react()],
+  i18n: {
+    locales: ["es", "en", "pt-br"],
+    defaultLocale: "pt-br",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  adapter: cloudflare(),
+});
